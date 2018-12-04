9f5a98e5f5afcbe5535825c22827476e3cc1437c
qjm201000 <1529062902@qq.com>
2018-12-04 18:23:31 +0800

    享元模式

--- src/com/pattern/Main.java
+++ src/com/pattern/Main.java
@@ -6,7 +6,7 @@ public class Main {
         article.setArticleTitle("圆周率的实现");
         article.setArticleContent("圆是一个圆圈。。。");
 
-        Subject subject = new Subject();//订阅号
+        subscibe subject = new subscibe();//订阅号
         subject.addObserver(new Observer1());//订阅者1
         subject.addObserver(new Observer2());//订阅者2
         subject.insertArticle(article);//订阅号新增一篇文章
--- src/com/pattern/subscibe.java
+++ src/com/pattern/subscibe.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 /**
  * 订阅号
  */
-public class Subject extends Observable {
+public class subscibe extends Observable {
     /**
      * 新增文章
      */
